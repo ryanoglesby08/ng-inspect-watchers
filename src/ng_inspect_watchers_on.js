249fbaf8f0255e1037afac35acf84db0a0a35e26
Ryan Oglesby <ryan.oglesby08@gmail.com>
2015-07-26 11:17:47 +0530

    Fix bug where window.angular might not exist on the page. Don't activate the extension in this case. version bump to 1.0.1

--- src/ng_inspect_watchers_on.js
+++ src/ng_inspect_watchers_on.js
@@ -1,117 +1,122 @@
 var main = function(angular, document) {
-  var ngWatchCount = function (targetElements) {
-    // ngWatchCount has been adopted from bahmutov (https://github.com/bahmutov/code-snippets/blob/master/ng-count-watchers.js)
-    //  with help from Patrick Caldwell (https://github.com/tncbbthositg). Thanks!
+  if( !angular ) {
+    console.log("[Ng Inspect Watchers] It seems that this page uses Angular, but I can't find the `window.angular` JavaScript object. Extension not activated.");
+  }
+  else {
+    var ngWatchCount = function (targetElements) {
+      // ngWatchCount has been adopted from bahmutov (https://github.com/bahmutov/code-snippets/blob/master/ng-count-watchers.js)
+      //  with help from Patrick Caldwell (https://github.com/tncbbthositg). Thanks!
 
-    var watchersOn = function(scope) {
-      if (scope && scope.$$watchers) {
-        return scope.$$watchers.length;
-      }
+      var watchersOn = function(scope) {
+        if (scope && scope.$$watchers) {
+          return scope.$$watchers.length;
+        }
 
-      return 0;
-    };
+        return 0;
+      };
 
-    var countWatchers = function(element) {
+      var countWatchers = function(element) {
+        var watchers = 0;
+        var angularElement = angular.element(element);
+
+        // Elements can be .ng-scope, .ng-isolate-scope, or both. I think if it is both,
+        //  then isolateScope() will have the $$watchers
+        if (angularElement.hasClass('ng-scope')) {
+          watchers += watchersOn(angularElement.scope());
+        }
+
+        if (angularElement.hasClass('ng-isolate-scope')) {
+          watchers += watchersOn(angularElement.isolateScope());
+        }
+
+        angular.forEach(angularElement.children(), function(childElement) {
+            watchers += countWatchers(childElement);
+        });
+
+        return watchers;
+      };
+
+
+      var angularTargetElements = angular.element(targetElements || document);
+
       var watchers = 0;
-      var angularElement = angular.element(element);
-
-      // Elements can be .ng-scope, .ng-isolate-scope, or both. I think if it is both,
-      //  then isolateScope() will have the $$watchers
-      if (angularElement.hasClass('ng-scope')) {
-        watchers += watchersOn(angularElement.scope());
-      }
-
-      if (angularElement.hasClass('ng-isolate-scope')) {
-        watchers += watchersOn(angularElement.isolateScope());
-      }
-
-      angular.forEach(angularElement.children(), function(childElement) {
-          watchers += countWatchers(childElement);
+      angular.forEach(angularTargetElements, function(element) {
+        watchers += countWatchers(element);
       });
+
+      console.log('[Ng Inspect Watchers] Element contains ' + watchers + ' watchers.');
 
       return watchers;
     };
 
+    var watcherCountClassName = "iw-ng-watcher-count";
+    var watcherHighlightClassName = "iw-ng-scope-highlight";
+    var watcherCountTemplate = "<div class='" + watcherCountClassName + "'>{watcherCount}</div>";
 
-    var angularTargetElements = angular.element(targetElements || document);
-
-    var watchers = 0;
-    angular.forEach(angularTargetElements, function(element) {
-      watchers += countWatchers(element);
-    });
-
-    console.log('[Ng Inspect Watchers] Element contains ' + watchers + ' watchers.');
-
-    return watchers;
-  };
-
-  var watcherCountClassName = "iw-ng-watcher-count";
-  var watcherHighlightClassName = "iw-ng-scope-highlight";
-  var watcherCountTemplate = "<div class='" + watcherCountClassName + "'>{watcherCount}</div>";
-
-  function removeAnyWatcherHighlighting() {
-    angular.element(document.querySelector('.' + watcherHighlightClassName)).removeClass(watcherHighlightClassName);
-    angular.element(document.querySelector('.' + watcherCountClassName)).remove();
-  }
-
-  function closestParentWithAScope(element) {
-    var parent = angular.element(element);
-
-    while( parent && !(parent.hasClass('ng-scope') || parent.hasClass('ng-isolate-scope')) ) {
-      parent = parent.parent();
+    function removeAnyWatcherHighlighting() {
+      angular.element(document.querySelector('.' + watcherHighlightClassName)).removeClass(watcherHighlightClassName);
+      angular.element(document.querySelector('.' + watcherCountClassName)).remove();
     }
 
-    return parent;
+    function closestParentWithAScope(element) {
+      var parent = angular.element(element);
+
+      while( parent && !(parent.hasClass('ng-scope') || parent.hasClass('ng-isolate-scope')) ) {
+        parent = parent.parent();
+      }
+
+      return parent;
+    }
+
+    function findScopeParentThen(element, workForScopeParent) {
+      var scopeParent = closestParentWithAScope(element);
+
+      workForScopeParent(scopeParent);
+    }
+
+    function highlight(element) {
+      element.addClass(watcherHighlightClassName);
+    }
+
+    function insertWatcherCountInto(element) {
+      var watcherCount = ngWatchCount(element);
+
+      element.append(watcherCountTemplate.replace('{watcherCount}', watcherCount));
+    }
+
+    var showWatchers = function(event) {
+      removeAnyWatcherHighlighting();
+
+      findScopeParentThen(event.target, function(scopeParent) {
+        highlight(scopeParent);
+        insertWatcherCountInto(scopeParent);
+      });
+
+      event.stopPropagation();
+    };
+
+    var hideWatchers = function(event) {
+      removeAnyWatcherHighlighting();
+
+      event.stopPropagation();
+    };
+
+    disableNgInspectWatchers = function() {
+      angular.element(document).off('mouseover', showWatchers)
+                               .off('mouseout', hideWatchers);
+
+      document.body.removeChild(document.getElementById('iw_inspect_watchers_js_on'));
+    };
+
+
+    angular.element(document).on('mouseover', showWatchers)
+                             .on('mouseout', hideWatchers);
   }
-
-  function findScopeParentThen(element, workForScopeParent) {
-    var scopeParent = closestParentWithAScope(element);
-
-    workForScopeParent(scopeParent);
-  }
-
-  function highlight(element) {
-    element.addClass(watcherHighlightClassName);
-  }
-
-  function insertWatcherCountInto(element) {
-    var watcherCount = ngWatchCount(element);
-
-    element.append(watcherCountTemplate.replace('{watcherCount}', watcherCount));
-  }
-
-  var showWatchers = function(event) {
-    removeAnyWatcherHighlighting();
-
-    findScopeParentThen(event.target, function(scopeParent) {
-      highlight(scopeParent);
-      insertWatcherCountInto(scopeParent);
-    });
-
-    event.stopPropagation();
-  };
-
-  var hideWatchers = function(event) {
-    removeAnyWatcherHighlighting();
-
-    event.stopPropagation();
-  };
-
-  disableNgInspectWatchers = function() {
-  angular.element(document).off('mouseover', showWatchers)
-                           .off('mouseout', hideWatchers);
-
-    document.body.removeChild(document.getElementById('iw_inspect_watchers_js_on'));
-  };
-
-
-  angular.element(document).on('mouseover', showWatchers)
-                           .on('mouseout', hideWatchers);
 };
 
 var script = document.createElement('script');
 script.id = 'iw_inspect_watchers_js_on';
 script.appendChild(document.createTextNode('var disableNgInspectWatchers = null;'));
-script.appendChild(document.createTextNode('('+ main +')(angular, document);'));
+script.appendChild(document.createTextNode('('+ main +')(window.angular, document);'));
 
 document.body.appendChild(script);
